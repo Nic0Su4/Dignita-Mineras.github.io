d7aa75d543ac6bf75fa58522ebc6dd1c4be249a0
Nic0Su4 <nicolas.suarez.vera@gmail.com>
2023-03-25 15:21:17 -0500

    Cambios sábado 25

--- scripts.js
+++ scripts.js
@@ -1,46 +1,76 @@
-/* Textos how */
-const iconos = document.querySelectorAll('.icono-how');
-const textos = document.querySelectorAll('.texto-how');
-const progreso = document.querySelector('.progress-bar');
-const barra = document.querySelector('.barra-how');
+// Te ayudamos
+const contenedor = document.querySelector(".empezar-terminar .contenido > div");
 
-for (let i = 0; i < iconos.length; i++) {
-    iconos[i].addEventListener('click', function() {
-        // Ocultamos todos los textos
-        for (let j = 0; j < textos.length; j++) {
-            textos[j].style.display = 'none';
+const progreso = document.querySelector(".empezar-terminar .progreso");
+
+let iconos = [];
+iconos = document.querySelectorAll(".empezar-terminar .iconos > div img");
+
+let barra = [];
+barra = document.querySelectorAll(".empezar-terminar .barra-res > div > div");
+
+let barraContenedor = [];
+barraContenedor = document.querySelectorAll(".empezar-terminar .barra-res > div");
+
+function icono(x) {
+  contenedor.style.transform = `translate(${x}%)`;
+
+  progreso.style.width = `${Math.abs(x)+25}%`;
+
+  
+
+  for (i=0; i<iconos.length; i++) {
+    if (i == Math.abs(x)/25) {
+      iconos[i].style.filter = "grayscale(0%)";
+    } else {
+      iconos[i].style.filter = "grayscale(100%)";
+    }
+  }
+
+  for (i=0; i<barra.length; i++) {
+    if (i == Math.abs(x)/25) {
+      barra[i].style.backgroundColor = "var(--colorPrimario)";
+    } else {
+      barra[i].style.backgroundColor = "var(--colorSecunadrio)";
+    }
+  }
+
+  for (i=0; i<barraContenedor.length; i++) {
+    if (i == Math.abs(x)/25) {
+      barraContenedor[i].style.backgroundColor = "var(--colorPrimario)";
+      barraContenedor[i].style.borderRadius = "0 20px 20px 0";
+
+      for (j=1; j<barraContenedor.length; j++) {
+        if (j == i) {
+          barraContenedor[j].style.backgroundColor = "var(--colorPrimario)";
+          barraContenedor[j-1].style.backgroundColor = "var(--colorPrimario)";
+          barraContenedor[j-1].style.borderRadius = "20px 0 0 20px";
+          barra[j-1].style.borderLeft = "none";
         }
-        // Mostramos el texto correspondiente
-        textos[i].style.display = 'block';
+      }
 
-        const tamanioBarra = {
-            0: '25%',
-            1: '50%',
-            2: '75%',
-            3: '100%'
-        }
-
-        progreso.style.width = tamanioBarra[i];
-    });
+    } else {
+      barraContenedor[i].style.backgroundColor = "var(--colorSecunadrio)";
+      barraContenedor[i].style.borderRadius = "0 0 0 0";
+    }
+  }
 }
-
-
 // Listas de procesos core
 const contenedorCards = document.querySelector(".cards-contenedor");
 
-contenedorCards.addEventListener("click", function(event) {
-    if (event.target.closest(".core .cards-contenedor > div")) {
-        let elemento;
-      if (event.target.tagName === "DIV") {
-        elemento = event.target;
-      }
-      else {
-        elemento = event.target.parentNode;
-      }
-
-      elemento.classList.toggle("select");
-
+contenedorCards.addEventListener("click", function (event) {
+  if (event.target.closest(".core .cards-contenedor > div")) {
+    let elemento;
+    if (event.target.tagName === "DIV") {
+      elemento = event.target;
     }
+    else {
+      elemento = event.target.parentNode;
+    }
+
+    elemento.classList.toggle("select");
+
+  }
 });
 
 // Listas de procesos general
@@ -54,16 +84,16 @@ let contenido = [];
 contenido = document.querySelectorAll(".general .contenido");
 
 for (let i = 0; i < div.length; i++) {
-  div[i].addEventListener("mouseenter", (function(index) {
-    return function() {
+  div[i].addEventListener("mouseenter", (function (index) {
+    return function () {
       portada[index].style.opacity = "0";
       portada[index].style.zIndex = "1";
       contenido[index].style.opacity = "1";
     }
   })(i))
 
-  div[i].addEventListener("mouseleave", (function(index) {
-    return function() {
+  div[i].addEventListener("mouseleave", (function (index) {
+    return function () {
       portada[index].style.opacity = "1";
       portada[index].style.zIndex = "3";
       contenido[index].style.opacity = "0";
@@ -72,11 +102,22 @@ for (let i = 0; i < div.length; i++) {
 }
 
 
-/* Animaciones scroll */
-// const viewportWidth = window.innerWidth;
+// const itemActivo = document.querySelector('.activo');
+// const item1 = document.querySelector('.item1');
+// const item3 = document.querySelector('.item3');
 
-// const swiperWrapper = document.querySelector('.swiper-wrapper');
-// const swiperContainer = document.querySelector('.swiper');
+// setInterval(() => {
+//   const itemActivo = document.querySelector('.activo');
+//   const item1 = document.querySelector('.item1');
+//   const item3 = document.querySelector('.item3');
+//   // Esperar a que termine la transición actual
+//   setTimeout(() => {
+//     itemActivo.classList.remove('activo');
+//     itemActivo.classList.add('item3');
+//     item1.classList.remove('item1');
+//     item1.classList.add('activo');
+//     item3.classList.remove('item3');
+//     item3.classList.add('item1');
+//   }, 500); // Duración de la transición en milisegundos
+// }, 2000);
 
-// swiperWrapper.style.width = `${viewportWidth - 60}px`;
-// swiperContainer.style.overflow = 'hidden';
